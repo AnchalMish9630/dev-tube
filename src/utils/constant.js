371cf770a3bfeb3b8973fe8a961fcb3c6d6b4123
AnchalMish9630 <anchalmish2025@gmail.com>
2025-03-08 22:02:28 +0530

    DT-1 Added buttonlist component

---
+++ src/utils/constant.js
@@ -0,0 +1,25 @@
+export const GOOGLE_API_KEY= "AIzaSyCXc0jeHSLb8DEPk7VzGd6wxWW_KhWHths"
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY
+// HTTP/1.1"
+
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q="
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
