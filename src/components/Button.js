b0d0cd040557ac07dc0bc251bb8143a22ddb3ff3
AnchalMish9630 <anchalmish2025@gmail.com>
2025-03-20 01:03:19 +0530

    DT-1 CSS changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 const Button = ({name}) =>{
     return(
         <div>
-            <button className="px-5 bg-gray-100 m-2 rounded-lg py-2  hover:bg-gray-200 active:bg-black active:text-white text-black">
-               {name}
+            <button className="flex w-auto lg:w-[80%] md:w-[73%]  [&::-webkit-scrollbar]:h-0">
+            {/* > */}
+              <div className="px-5 bg-gray-100 m-2 rounded-lg py-2  hover:bg-gray-200 active:bg-black active:text-white text-black">{name}</div>
+               {/* {name} */}
             </button>
         </div>
     )
