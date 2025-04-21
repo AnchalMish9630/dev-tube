fcc3da610f3878fda1fc1e90678eb7f288a5ffa6
AnchalMish9630 <anchalmish2025@gmail.com>
2025-04-21 23:52:12 +0530

    DT-1 Login page

---
+++ src/components/UserLogin .js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+export const UserLogin  = () => {
+    const [isSignInForm , setIsSignInForm ] = useState(true);
+    const [message, setMessage] = useState(null);
+    const handleSignInForm = ()=>{
+        setIsSignInForm(!isSignInForm);
+    }
+  return (
+    <div>
+         <div> 
+ 
+                <div className="absolute">
+                <img className="w-full"
+                // src={BG_IMG_URL}
+                alt= "bg-img"
+                />
+                </div>
+                <form className="w-3/12 absolute my-36 mx-auto p-12 bg-[#464657] right-0 left-0 text-white rounded-lg bg-opacity-95"
+                    onSubmit={(e)=>e.preventDefault()}
+                    >
+                    <h1 className="font-bold text-[36px]">
+                        {isSignInForm ? "Sign in" : "Sign up"} 
+                          </h1>
+                    {!isSignInForm &&  
+                     <input 
+                    //  ref={name} 
+                           type="text"
+                           placeholder="Enter full name" 
+                           className="p-4 my-4 w-full bg-[#464657] rounded-md" 
+                
+                     />
+                    }
+                    <input 
+                    // ref={email}
+                           type="text"
+                           placeholder="Enter email address" 
+                           className="p-4 my-4 w-full bg-[#464657] rounded-md" 
+                    />
+                    <input 
+                    //  ref={password}   // by this get the refrence of the input box.
+                            type="password" 
+                            placeholder="Enter password" 
+                            className="p-4 my-4 w-full bg-[#464657] rounded-md"
+                    />
+                   <p className="font-bold text-lg text-[#464657] py-2">{message}</p>
+                    <button 
+                            className="p-4 my-6 w-full rounded-lg bg-red-700"
+                            // onClick={handleButtonClick}
+                        >
+                         {
+                            isSignInForm ? "Sign in" : "Sign up" 
+                        } 
+                    </button>
+                    <p className="py-4 cursor-pointer"
+                     onClick={handleSignInForm}
+                     >
+                        {isSignInForm ? "New to Youtube? Sign Up now." : "Already registered? Sign in now."} 
+                    </p>
+                </form>
+               
+        </div>
+    </div>
+  )
+}
+export default UserLogin;
