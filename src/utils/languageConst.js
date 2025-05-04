b4af6d5762c7485c87ba37748aa1ba8038a22633
AnchalMish9630 <anchalmish2025@gmail.com>
2025-05-04 23:54:27 +0530

    DT-1 Added translation for texts

--- src/utils/languageConst.js
+++ src/utils/languageConst.js
@@ -1,10 +1,43 @@
+
 const lang = {
     en:{
-
-        
+        home: "Home",
+        history: "History",
+        subscription:"Subscriptions", 
+        likedVideo: "Liked Videos",
+        signIn: "Sign in",
+        all: "All",
+        music:"Music",
+        lofi: "Lofi",
+        jukeBox:"JukeBox",
+        javascript: "Javascript",
+        dataStructure: "DataStructure",
+        Cricket: "Cricket",
+        bhakti:"Bhakti",
+        react: "React",
+        coding: "Coding",
+        chant :"Chants", 
+        motivation: "Motivation"
     },
     hi:{
-
+            home: "home",
+            history: "इतिहास",
+            subscription:"सदस्यता", 
+            likedVideo: " वीडियो",
+            signIn: "साइन इन करें",
+            all: "सभी",
+            music:"संगीत",
+            lofi: "लोफी",
+            jukeBox:" ज्यूकबॉक्स",
+            javascript: "जावास्क्रिप्ट",
+            dataStructure: "डेटास्ट्रक्चर",
+            Cricket: "क्रिकेट",
+            bhakti:"भक्ति",
+            react: "प्रतिक्रिया",
+            coding: "कोडिंग",
+            chant:"मंत्र", 
+            motivation: "प्रेरणा"
+          
     }
 
 }
