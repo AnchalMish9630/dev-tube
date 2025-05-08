dd65d4f19b0d6b52c9583ca6e6d2cb77b4a4eeb6
AnchalMish9630 <anchalmish2025@gmail.com>
2025-05-08 23:59:08 +0530

    DT-1 Langugage config

---
+++ src/utils/i18n/languageKeys.js
@@ -0,0 +1,24 @@
+export const LANGUAGE_KEYS = {
+    ALL: "all",
+    MUSIC: "music",
+    LOFI: "lofi",
+    JUKEBOX: "jukeBox",
+    JAVASCRIPT: "javascript",
+    DATA_STRUCTURE: "dataStructure",
+    CRICKET: "Cricket",
+    BHAKTI: "bhakti",
+    REACT: "react",
+    CODING: "coding",
+    CHANT: "chant",
+    MOTIVATION: "motivation",
+    HOME: "home",
+    HISTORY:"history",
+    SUBSCRIPTION:"subscription",
+    LIKED_VIDEOS: "likedVideo",
+    SIGN_IN:"signIn",
+    SEARCH: "search"
+
+  };
+
+//   | ✅ Clean | ✅ Scalable | ✅ Easy to Update | ✅ Reusable | ✅ Safe |
+// | ------- | ---------- | ---------------- | ---------- | ------ |
