9b0107ff94457d0a99db963e662fa0f171cae1f9
AnchalMish9630 <anchalmish2025@gmail.com>
2025-06-11 23:24:58 +0530

    DT-1 Added chatslice to store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
 import searchSlice from "./searchSlice";
-import configSlice from "./configSlice"
+import configSlice from "./configSlice";
+import chatSlice from "./chatSlice";
 
 
 const appStore = configureStore({
@@ -9,6 +10,7 @@ const appStore = configureStore({
         app: appSlice,
         search: searchSlice,
         config: configSlice,
+        chat: chatSlice
     }
 })
  export default appStore;
