1ee174622ec31ed26e3a0558f9585fbae59c9d6f
AnchalMish9630 <anchalmish2025@gmail.com>
2025-06-25 23:53:31 +0530

    DT-1 Header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { lang } from "../utils/i18n";
 import { changeLang } from "../utils/configSlice";
 import { LANGUAGE_KEYS } from "../utils/i18n/languageKeys";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -25,41 +26,41 @@ const Header = () => {
 
   const langCode = useSelector((store)=>store.config.lang)
 
-  const getSearchSuggestion = async () => {
-    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-    const jsonData = await data.json();
-    console.log("data is searched", jsonData[1]);
-    if (jsonData && jsonData[1]) {
-      setSuggestionResult(jsonData[1]); // Update local suggestion result
-      dispatch(cacheResult({ [searchQuery]: jsonData[1] }));
-    }
-  };
+  // const getSearchSuggestion = async () => {
+  //   const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+  //   const jsonData = await data.json();
+  //   console.log("data is searched", jsonData[1]);
+  //   if (jsonData && jsonData[1]) {
+  //     setSuggestionResult(jsonData[1]); // Update local suggestion result
+  //     dispatch(cacheResult({ [searchQuery]: jsonData[1] }));
+  //   }
+  // };
 
-  useEffect(() => {
-    {
-      if (searchCache[searchQuery]) {
-        setSuggestionResult(searchCache[searchQuery]);
-      } else {
-        const timer = setTimeout(() => getSearchSuggestion(), 200);
-        return () => {
-          clearTimeout(timer);
-        };
-      }
-    }
-  }, [searchQuery, searchCache]); // Trigger whenever searchQuery or searchCache changes
+  // useEffect(() => {
+  //   {
+  //     if (searchCache[searchQuery]) {
+  //       setSuggestionResult(searchCache[searchQuery]);
+  //     } else {
+  //       const timer = setTimeout(() => getSearchSuggestion(), 200);
+  //       return () => {
+  //         clearTimeout(timer);
+  //       };
+  //     }
+  //   }
+  // }, [searchQuery, searchCache]); // Trigger whenever searchQuery or searchCache changes
 
   return (
-    <div className=" flex w-full justify-between items-center px-6 static z-20 lg:fixed bg-white ">
+    <div className=" flex w-full justify-between items-center md:px-6 font-semibold px-1 static z-20 lg:fixed bg-white md:border-none border-b border-gray-200">
       <div className="w-[10rem] flex flex-nowrap">
         <img
           className="hidden md:inline-block w-9 h-12 pt-4 cursor-pointer"
           onClick={() => handleToggleMenu()}
           alt="menu-icon"
-          src={MENU_ICON} 
+           src={MENU_ICON} 
         />
         <a href="/">
           <img
-            className="w-28 h-16 cursor-pointer"
+            className="md:w-28 md:h-16 w-25 h-14 block cursor-pointer"
             src={YOUTUBE_LOGO}
             alt="yt-logo"
           />
@@ -81,6 +82,7 @@ const Header = () => {
             {lang[langCode].search}
           </button>
         </div>
+        
 
         {showSuggestion && (
           <div className="fixed bg-white border border-gray-200 py-2 px-5 w-96 shadow-lg rounded-lg mt-10  lg:w-[30rem] [@media(max-width:768px)]:w-[10rem] md:w-[15rem]" >
@@ -94,8 +96,15 @@ const Header = () => {
             </ul>
           </div>
         )}
+
+
       </div>
-      <div className="flex flex-col" >
+      <div className="md:hidden flex ml-24 px-2">
+  <button className="text-gray-700 p-2">
+    <FaSearch size={20} />
+  </button>
+</div>
+      <div className="hidden md:flex flex-col" >
        <select onChange={handleLanguageChange}>
         {
           SUPPORTED_LANG.map((lang)=>{
@@ -108,7 +117,7 @@ const Header = () => {
         // onClick={()=>{navigate("/watch")
         //   // navigate("/browse");
         // }}
-        className="flex justify-between items-center flex-nowrap border border-gray-200 rounded-full cursor-pointer hover:bg-blue-100">
+        className="hidden md:flex justify-between items-center flex-nowrap border border-gray-200 rounded-full cursor-pointer hover:bg-blue-100">
         <img
           className="h-8 pl-2 "
           src={USER_IMG}
