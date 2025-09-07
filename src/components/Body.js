eefd074d6e077b4bff6750783c5dd9313fce4f2d
AnchalMish9630 <anchalmish2025@gmail.com>
2025-09-07 23:49:00 +0530

    Header

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,12 @@ import React from 'react'
 import SideBar from './Sidebar'
 import MainContainer from './MainContainer'
 import { Outlet } from 'react-router-dom'
+import Header from './Header'
 
 const Body = () => {
   return (
     <div className=''>
+      <Header />
       <SideBar />
       <Outlet />
     </div>
