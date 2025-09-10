f1ccf33419b2a85e4fd6d7f2f66d4e2b76fcd7b6
AnchalMish9630 <anchalmish2025@gmail.com>
2025-09-10 23:51:10 +0530

    added adds component

---
+++ src/components/Adds.js
@@ -0,0 +1,11 @@
+
+const Adds = ()=>{
+
+  return <>
+    first we will pss props if it is coming from some other component 
+    for definng props , we need to defince its type , if we are using typscript.
+    then 
+  </>
+
+}
+export default Adds;
